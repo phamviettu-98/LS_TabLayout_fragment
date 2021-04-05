4c2821cd9edea824a4409d3f65d7c40421c6afcf
quytladean <phamviettu567@gmail.com>
2021-04-05 20:25:52 +0700

    Push User.java

---
+++ app/src/main/java/viettu/pvt/ls_tablayout_fragment/Acount.java
@@ -0,0 +1,27 @@
+package viettu.pvt.ls_tablayout_fragment;
+
+public class Acount {
+    private String username;
+    private String password;
+
+    public Acount(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
